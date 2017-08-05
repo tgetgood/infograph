4cd5e136c83f0e72f0769c5dececb5aea9b6e0ba
Thomas Getgood <thomas.getgood@gmail.com>
2017-08-05 19:43:23 -0400

    Reimplemented geometry.
    This time using names of things correctly.

--- src/infograph/geometry.cljs
+++ src/infograph/geometry.cljs
@@ -1,15 +1,24 @@
 (ns infograph.geometry
   "Geometric Utilities.")
 
-(defn norm
-  "Returns the Euclidean norm of two points represented as vectors.
-  N.B.: Currently only implemented in 2 dimensions"
+(defn dot
+  "Returns the Euclidean inner product of p and q."
   [p q]
   {:pre [(every? #(and (number? %) (not (js/isNaN %))) (concat p q))]}
-  (when (and p q)
-    (js/Math.sqrt
-     (reduce (fn [acc [a b]]
-               (let [d (- a b)]
-                 (+ acc (* d d))))
-             0
-             (map vector p q)))))
+  (reduce + (map * p q)))
+
+(defn norm
+  "Returns the norm (length) of vector v in Euclidean space."
+  [v]
+  (when v
+    (js/Math.sqrt (dot v v))))
+
+;; REVIEW: It feels like implementing addition on vectors for this would be bad
+;; form, but it would also be so clean looking...
+(defn sub [[x1 y1] [x2 y2]]
+  [(- x1 x2) (- y1 y2)])
+
+(defn dist
+  "Returns the distance between p and q in R^n"
+  [p q]
+  (norm (sub q p)))
